fe70614a09e7de3e66ff6ba5b20c6e647933da1a
pbxyz <pablexyz@hotmail.com>
2021-02-06 01:20:18 -0430

    14

--- 002.js
+++ 002.js
@@ -2,4 +2,4 @@ function getFinalPrice(price, discount) {
     return price - price / discount;
 }
 console.log(getFinalPrice(100, 10));
-console.log(getFinalPrice(100, "10%"));
+// console.log(getFinalPrice(100, "10%"));
