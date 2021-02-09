aed88a8d92925b797763b7cbb3242eb1f72a4024
pbxyz <pablexyz@hotmail.com>
2021-02-09 15:50:01 -0430

    23

---
+++ src/005.js
@@ -0,0 +1,10 @@
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var p = new Person();
+p.firstName = "Jhon";
+p.lastName = "Smith";
+p.age = 25;
+console.log(p);
---
+++ src/005.ts
@@ -0,0 +1,12 @@
+class Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+const p = new Person();
+p.firstName = "Jhon";
+p.lastName = "Smith";
+p.age = 25;
+
+console.log(p);
