94866d5eb4fc0317f1e58a6148039461f701318f
pbxyz <pablexyz@hotmail.com>
2021-02-09 22:23:04 -0430

    24

--- src/005.js
+++ src/005.js
@@ -1,10 +1,34 @@
-var Person = /** @class */ (function () {
-    function Person() {
-    }
-    return Person;
-}());
-var p = new Person();
-p.firstName = "Jhon";
-p.lastName = "Smith";
-p.age = 25;
-console.log(p);
+function savePerson(person) {
+    console.log("Saving ", person);
+}
+var p = {
+    firstName: "John",
+    lastName: "Smith",
+    age: 25
+};
+savePerson(p);
+// // class Person {
+// //     firstName: string;
+// //     lastName: string;
+// //     age: number;
+// // }
+// // const p = new Person();
+// // p.firstName = "Jhon";
+// // p.lastName = "Smith";
+// // p.age = 25;
+// // console.log(p);
+// class Block {
+//     readonly nonce: number;
+//     readonly hash: string;
+//     constructor(
+//         readonly index: number,
+//         readonly previousHash: string,
+//         readonly timestamp: number,
+//         readonly data: string
+//     ) {
+//         const { nonce, hash } = this.mine();
+//         this.nonce = nonce;
+//         this.hash = hash;
+//     }
+//     // The rest of the code is omitted for brevity
+// }
--- src/005.ts
+++ src/005.ts
@@ -1,12 +1,46 @@
-class Person {
+interface Person {
     firstName: string;
     lastName: string;
     age: number;
 }
 
-const p = new Person();
-p.firstName = "Jhon";
-p.lastName = "Smith";
-p.age = 25;
+function savePerson(person: Person): void {
+    console.log("Saving ", person);
+}
 
-console.log(p);
+const p: Person = {
+    firstName: "John",
+    lastName: "Smith",
+    age: 25,
+};
+
+savePerson(p)
+
+// // class Person {
+// //     firstName: string;
+// //     lastName: string;
+// //     age: number;
+// // }
+
+// // const p = new Person();
+// // p.firstName = "Jhon";
+// // p.lastName = "Smith";
+// // p.age = 25;
+
+// // console.log(p);
+
+// class Block {
+//     readonly nonce: number;
+//     readonly hash: string;
+//     constructor(
+//         readonly index: number,
+//         readonly previousHash: string,
+//         readonly timestamp: number,
+//         readonly data: string
+//     ) {
+//         const { nonce, hash } = this.mine();
+//         this.nonce = nonce;
+//         this.hash = hash;
+//     }
+//     // The rest of the code is omitted for brevity
+// }
