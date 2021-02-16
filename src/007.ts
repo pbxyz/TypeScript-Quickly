0b329befed231c2d112d4d8845d2594bed3c9acb
pbxyz <pablexyz@hotmail.com>
2021-02-15 19:31:52 -0430

    30

--- src/007.ts
+++ src/007.ts
@@ -1,12 +1,33 @@
-interface A {
-    a: number;
-}
-interface B {
-    b: string;
-}
-function foo(x: A | B) {
-    if ("a" in x) {
-        return x.a;
+class Dog {
+    constructor(readonly name: string) {}
+
+    sayHello(): string {
+        return "Dog says hello!";
     }
-    return x.b;
 }
+
+class Fish {
+    constructor(readonly name: string) {}
+
+    dive(howDeep: number): string {
+        return `Diving ${howDeep} feet`;
+    }
+}
+
+type Pet = Dog | Fish;
+
+function talkToPet(pet: Pet): string {
+    if (pet instanceof Dog) {
+        return pet.sayHello();
+    } else if (pet instanceof Fish) {
+        return "Fish cannot talk, sorry.";
+    }
+}
+
+const myDog = new Dog("Sammy");
+
+const myFish = new Fish("Marry");
+
+console.log(talkToPet(myDog));
+console.log(talkToPet(myFish));
+//talkToPet({ name: 'John' });
